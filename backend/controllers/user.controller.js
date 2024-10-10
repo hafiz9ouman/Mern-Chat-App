2888f3195e61586e4c81dbb9f8e1bf6f9f30b485
hafiz9ouman <hafiz9oman.dev@gmail.com>
2024-10-10 16:47:56 +0500

    sidebar user api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,14 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
     try {
-        res.send("ok");
+        const loggedInUserId = req.user._id;
+
+        const allUsers = await User.find({
+            _id: { $ne: loggedInUserId }
+        });
+
+        res.status(200).json(allUsers);
     } catch (error) {
         console.log("Error in getUsersForSidebar", error.message);
         return res.status(500).json({error: "Internal Server Error"});
